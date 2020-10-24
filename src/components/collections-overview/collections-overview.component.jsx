2d779b9868ca171c2a8afb0b6ff7c555b52bdf02
Rhett Harrison <reharri7@asu.edu>
2020-10-23 19:10:37 -0700

    Revert "Summary"
    This reverts commit c9f4ccca1d8bf540951885ce3184d3d993fd2f11.

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,24 +1,21 @@
-  
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import { selectCollections } from '../../redux/shop/shop.selectors';
 
-import CollectionPreview from '../collection-preview/collection-preview.component';
-
-import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
-
+import CollectionPreview from '../collection-preview/collection-preview';
 import './collections-overview.styles.scss';
 
-const CollectionsOverview = ({ collections }) => (
-  <div className='collections-overview'>
-    {collections.map(({ id, ...otherCollectionProps }) => (
-      <CollectionPreview key={id} {...otherCollectionProps} />
-    ))}
-  </div>
+const collectionsOverview = ({ collections }) => (
+    <div className='collections-overview'>
+        {collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
+    </div>
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollectionsForPreview
+    collections: selectCollections,
 });
 
-export default connect(mapStateToProps)(CollectionsOverview);
+export default connect(mapStateToProps, null)(collectionsOverview);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -7,8 +7,10 @@ import CollectionPage from '../collection/collection.component';
 const ShopPage = ({ match }) => (
   <div className='shop-page'>
     <Route exact path={`${match.path}`} component={CollectionsOverview} />
-    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+    <Route path={`${match.path}/:CollectionId`} component={CollectionPage} />
   </div>
 );
+
+
 
 export default ShopPage;
